13c437318037bbaf2306ef8c3449d2e7ac5e1e2e
QBshooter <93053980+QBshooter@users.noreply.github.com>
2024-02-28 08:29:55 +0100

    labo 16 Quinten Bernard 1TId

--- labo_16/Opdracht Producten/scripts/code.js
+++ labo_16/Opdracht Producten/scripts/code.js
@@ -8,13 +8,18 @@ let bereken = () =>{
     let aantal = document.getElementsByClassName("aantal");
     let btw = document.getElementsByClassName("btw");
     let subtotaal = document.getElementsByClassName("subtotaal");
+    let totaal = document.getElementById("totaal");
+    console.log(parseFloat(prijs[1].textContent));
+    console.log(aantal[1].value);
+    console.log(parseInt(btw[1].textContent,10));
+    let subtotaalBerekend1 = ((parseFloat(prijs[0].textContent))*aantal[0].value)+((parseFloat(prijs[0].textContent)*aantal[0].value)*(parseInt(btw[0].textContent,10))/100);
+    let subtotaalBerekend2 = ((parseFloat(prijs[1].textContent))*aantal[1].value)+((parseFloat(prijs[1].textContent)*aantal[1].value)*(parseInt(btw[1].textContent,10))/100);
+    let subtotaalBerekend3 = ((parseFloat(prijs[2].textContent))*aantal[2].value)+((parseFloat(prijs[2].textContent)*aantal[2].value)*(parseInt(btw[2].textContent,10))/100);
+    subtotaal[0].textContent=subtotaalBerekend1.toFixed(2)+' Eur';
+    subtotaal[1].textContent=subtotaalBerekend2.toFixed(2)+' Eur';
+    subtotaal[2].textContent=subtotaalBerekend3.toFixed(2)+' Eur';
+    totaal.textContent = (subtotaalBerekend1+subtotaalBerekend2+subtotaalBerekend3).toFixed(2) +" Eur"
 
-    let subtotaalBerekend1 = (parseFloat(prijs[0].textContent)*aantal[0])+((parseFloat(prijs[0].textContent)*aantal[0])*(parseInt(btw[0].textContent,10))/100);
-    let subtotaalBerekend2 = (prijs[1]*aantal[1])+((prijs[1]*aantal[1])*btw[1]/100);
-    let subtotaalBerekend3 = (prijs[2]*aantal[2])+((prijs[2]*aantal[2])*btw[2]/100);
-    subtotaal[0].textContent=''+subtotaalBerekend1+' Eur';
-    subtotaal[1].textConten=''+subtotaalBerekend2+' Eur';
-    subtotaal[2].textContet=''+subtotaalBerekend3+' Eur';
 
 
 }
