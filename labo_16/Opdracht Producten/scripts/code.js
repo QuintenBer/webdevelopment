23b43110c269ae07b3c30d2a944ad96c80904f31
QBshooter <93053980+QBshooter@users.noreply.github.com>
2024-02-29 11:27:30 +0100

    labo 16 Quinten Bernard 1TId

--- labo_16/Opdracht Producten/scripts/code.js
+++ labo_16/Opdracht Producten/scripts/code.js
@@ -9,9 +9,7 @@ let bereken = () =>{
     let btw = document.getElementsByClassName("btw");
     let subtotaal = document.getElementsByClassName("subtotaal");
     let totaal = document.getElementById("totaal");
-    console.log(parseFloat(prijs[1].textContent));
-    console.log(aantal[1].value);
-    console.log(parseInt(btw[1].textContent,10));
+
     let subtotaalBerekend1 = ((parseFloat(prijs[0].textContent))*aantal[0].value)+((parseFloat(prijs[0].textContent)*aantal[0].value)*(parseInt(btw[0].textContent,10))/100);
     let subtotaalBerekend2 = ((parseFloat(prijs[1].textContent))*aantal[1].value)+((parseFloat(prijs[1].textContent)*aantal[1].value)*(parseInt(btw[1].textContent,10))/100);
     let subtotaalBerekend3 = ((parseFloat(prijs[2].textContent))*aantal[2].value)+((parseFloat(prijs[2].textContent)*aantal[2].value)*(parseInt(btw[2].textContent,10))/100);
